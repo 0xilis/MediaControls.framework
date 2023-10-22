d07f50505ad9353f2d79eabcea50cc1a2e9570dc
0xilis <109871561+0xilis@users.noreply.github.com>
2023-10-21 20:58:40 -0500

    MRUNowPlayingView.m

---
+++ MRUNowPlayingView.m
@@ -0,0 +1,20 @@
+#import "MRUNowPlayingView.h"
+
+@implementation MRUNowPlayingView
+-(instancetype)initWithFrame:(CGRect)frame {
+ self = [super initWithFrame:frame];
+ if (self) {
+  [self setClipsToBounds:YES];
+  MRUNowPlayingControlsView *nowPlayingControlsView = [[MRUNowPlayingControlsView alloc]init];
+  _controlsView = nowPlayingControlsView;
+  [self addSubview:_controlsView];
+  MRUNowPlayingContainerView *nowPlayingContainerView = rax = [[MRUNowPlayingContainerView alloc]init];
+  _containerView = nowPlayingContainerView;
+  [self addSubview:_containerView];
+  MRUEmbeddingView *embeddingView = [[MRUEmbeddingView alloc]init];
+  _collapsedEmbeddingView = embeddingView;
+  [_containerView setContentView:_collapsedEmbeddingView];
+ }
+ return self;
+}
+@end
